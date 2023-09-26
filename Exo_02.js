aa48b4111dc6816d697fa5f84c91284e670b0dfd
MateoDedi <mateod@live.com>
2023-09-26 10:09:30 +0200

    added 02

---
+++ Exo_02.js
@@ -0,0 +1,19 @@
+function numDigits(a) {
+    let count = 0;
+    if (typeof a === 'number') {
+        let str = a.toString();
+        for (var i = 0; i < str.length; i++) {
+        }
+    } else {
+        return "Not a number...";
+    }
+
+    return count += i;
+}
+
+console.log(numDigits(1000));
+console.log(numDigits(12));
+console.log(numDigits(1305981031));
+console.log(numDigits(0));
+console.log(numDigits("lol"));
+console.log(numDigits());
