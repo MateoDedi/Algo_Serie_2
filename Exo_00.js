5c3e97acc3c84cff0a89620e6d81ee95c1f4499c
MateoDedi <mateod@live.com>
2023-09-26 09:16:42 +0200

    added exo 00

---
+++ Exo_00.js
@@ -0,0 +1,13 @@
+function countTrue(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === true) {
+            count += i;
+        }
+    }
+    return count;
+}
+
+console.log(countTrue([true, false, false, true, false]));
+console.log(countTrue([false, false, false, false]));
+console.log(countTrue([]));
