871e4d94472ad970dbc56e91b1dadc03c2fdb011
MateoDedi <mateod@live.com>
2023-09-27 11:45:37 +0200

    added 07

---
+++ Exo_07.js
@@ -0,0 +1,22 @@
+function calculator (num1, oprt ,num2) {
+
+    if (oprt === "+") {
+        return num1 + num2;
+    } else if (oprt === "-") {
+        return num1 - num2;
+    } else if (oprt === "*") {
+        return num1 * num2;
+    } else if (oprt === "/") {
+        if (num1 == 0 || num2 == 0) {
+            return "Can't divide by 0!"
+        } else {
+            return num1 / num2;
+        }
+    } else {
+        return "Enter a valid operator";
+    }
+}
+
+console.log(calculator(2, "+", 2) );
+console.log(calculator(2, "*", 2));
+console.log(calculator(4, "/", 2));
